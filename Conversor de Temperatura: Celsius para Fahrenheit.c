cb3b9f4fe5b5ecd5cf6ee0f82418a86e26ef225b
bauerfest <accesar@faeterj-petropolis.edu.br>
2024-10-04 16:53:34 -0300

    Update Conversor de Temperatura: Celsius para Fahrenheit.c

--- Conversor de Temperatura: Celsius para Fahrenheit.c
+++ Conversor de Temperatura: Celsius para Fahrenheit.c
@@ -4,10 +4,10 @@ int main(){
 
 int c;
 double f;
-printf("insira sa temperatura:");
+printf("insira sua temperatura em F°:");
 scanf("%d", &c);
 f=(c*1.8+32);
-printf(" a temperatura em f eh: %lf\n graus", f);
+printf(" a temperatura em f eh: %lf\n °", f);
 
 
 
