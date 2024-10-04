e7563fcbd246b66aa1767dcb6d7163e7535ac6ac
bauerfest <accesar@faeterj-petropolis.edu.br>
2024-10-04 15:12:18 -0300

    Add files via upload

---
+++ valor x em multiplas potencias.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+
+
+float x,x1,x2,x3,x4;
+printf("Insira o valor de x:");
+scanf("%f", &x);
+x1=(pow(x, 2));
+x2=(pow(x, 4));
+x3=(pow(x, 6));
+x4=(pow(x, 8));
+
+
+printf("O valor de x na  potencia de 2 eh : %.4f\n",x1);
+printf("O valor de x na  potencia de 4 eh : %.4f\n",x2);
+printf("O valor de x na  potencia de 6 eh : %.4f\n",x3);
+printf("O valor de x na  potencia de 8 eh : %.4f\n",x4);
+ return 0;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
